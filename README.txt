6595e6345282c749b50ea735805082792182f61f
Warrior2612 <prathamm2002@gmail.com>
2021-06-01 16:45:01 +0530

    v2

--- README.txt
+++ README.txt
@@ -1,2 +1,4 @@
 Checkout the project at:
 https://github.com/Warrior2612/Stock-Market-Data-Visualiser
+
+Create a virtual environment named smdv-env if you want to use RUN.bat and other scripts
--- RUN.bat
+++ RUN.bat
@@ -1,3 +1,44 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFCtGTRCKPWaGIrof/eX+4f6Unm4SUOcPaoDR37eaM9wj81HsepgA33RTqNkDBh5bai6iYw4zrH1+umuCOImVsACB
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFCtGTRCKPWaGIrof/eX+4f6Unm4SUOcPaoDR37eaM9wj81HsepgA33RTqNkDBh5bai6ZWyF6rHZH1g==
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+if exist smdv-env\ (
+  echo Yes
+) else (
+  pip install virtualenv
+  virtualenv smdv-env
+  CALL smdv-env/Scripts/activate.bat
+  pip install -r requirements.txt
+)
 CALL smdv-env/Scripts/activate.bat
 start pythonw.exe main.pyw
 exit
